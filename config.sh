6bb54ec945067ed548b50162310fd91b99d923d6
lee <lee.3326@qq.com>
2023-04-28 12:05:36 +0800

    skip include gtest in benchmark

--- config.sh
+++ config.sh
@@ -35,7 +35,7 @@ install_benchmark() {
     # Make a build directory to place the build output.
     cmake -E make_directory "build"
     # Generate build system files with cmake, and download any dependencies.
-    cmake -E chdir "build" cmake -DBENCHMARK_DOWNLOAD_DEPENDENCIES=on -DCMAKE_BUILD_TYPE=Release ../
+    cmake -E chdir "build" cmake  -DCMAKE_BUILD_TYPE=Release ../
     # or, starting with CMake 3.13, use a simpler form:
     # cmake -DCMAKE_BUILD_TYPE=Release -S . -B "build"
     # Build the library.
---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CURRENT_DIR=$(cd $(dirname $0); pwd)
+TEST_ROOT=$CURRENT_DIR/build/bin/test
+PROJECT_ROOT=$CURRENT_DIR
+mkdir $PROJECT_ROOT/build
+cmake -S ./ -B ./build
+cd $PROJECT_ROOT/build&&make&& cd $CURRENT_DIR
+echo -------------googletestDependTest----------------
+$TEST_ROOT/Test2
+echo -------------benchTest----------------
+$TEST_ROOT/benchTest
